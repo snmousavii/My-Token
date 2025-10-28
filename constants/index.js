d290732a051e5648bf9f1a4878bb399753ed2601
snmousavi <89077940+snmousavii@users.noreply.github.com>
2025-10-28 18:47:02 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -4,7 +4,7 @@ export const TOKEN_CONTRACT_ABI = [
 		"anonymous": false,
 		"inputs": [
 			{
-				"indexed": true,
+				
 				"internalType": "address",
 				"name": "owner",
 				"type": "address"
